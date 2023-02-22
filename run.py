b1f18092df819114a0b9cc64a9080dce583d352a
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-02-22 13:46:46 +0000

    foundations for the welcome strings & starting to add layut to board

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import gspread
+import random 
+
+# Import random package to be able to create random integers within the game
+
+def render (board_Width, board_height): 
+        print ( "+" + "-" * board_Width + "+")
+
+if __name__ == "__main__":
+    render (10, 10)
+
+print('Hello, Welcome to Battleships2023!')
+ans = input ('Are you ready to play (yes/no)')
+if ans.lower () == 'yes':
+    print('Ok, lets do this' )
+else: 
+    print('Ok, Come back when you are ready') 
+
+ #  User friendly interaction before starting game. 
+
+    inp= input ("Where do you want to shoot on the board?\n")
+    xstr, ystr = insp.split (",")
+    x = int(xstr)
+    y = int(ystr)
+    print (x)
+    print (y)
+    print (x + y)
+
+    
