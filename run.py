6d771a8879b674ee5c42d7b606e2a8b8cdfdbab1
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-02-22 14:54:03 +0000

    Added the board to the game

--- run.py
+++ run.py
@@ -4,8 +4,13 @@ import random
 
 # Import random package to be able to create random integers within the game
 
-def render (board_Width, board_height): 
-        print ( "+" + "-" * board_Width + "+")
+def render (board_width, board_height): 
+    header  = "#" + "-"* board_width + "#"
+    print(header)
+
+    for i in range(board_height):
+        print("|" + " "* board_width + "|")
+    print(header)
 
 if __name__ == "__main__":
     render (10, 10)
@@ -18,8 +23,8 @@ else:
     print('Ok, Come back when you are ready') 
 
  #  User friendly interaction before starting game. 
-
-    inp= input ("Where do you want to shoot on the board?\n")
+def where_to_shoot ():
+    inp = input ("Where do you want to shoot on the board?\n")
     xstr, ystr = insp.split (",")
     x = int(xstr)
     y = int(ystr)
