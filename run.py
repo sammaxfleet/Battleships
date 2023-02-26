63411b86394cd4d4864b9aab559e18ebedb133ae
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-02-26 19:17:12 +0000

    making structure of battleship

--- run.py
+++ run.py
@@ -4,32 +4,66 @@ import random
 
 # Import random package to be able to create random integers within the game
 
-def render (board_width, board_height): 
+ #  User friendly interaction before starting game.
+class Battleship(object):  
+    @staticmethod
+    def build(head, length, direction):
+        body = []
+        for i in range(length):
+            if direction == "N":
+                el = (head[0], head[1] - i)
+            elif direction == "S":
+                el = (head[0], head[1] + i)
+            elif direction == "W":
+                el = (head[0] - i, head[1])
+            elif direction == "E":
+                el = (head[0] + i, head[1])
+
+            body.append(el)
+        return Battleship(body)
+
+    def __init__(self, body):
+        self.body = body 
+
+b = Battleship.build((1,1), 5, "S")
+b2 = Battleship ([(1,2), (2,2), (3,3), (4,2), (5,2)])
+
+
+
+def board (board_width, board_height, shots): 
     header  = "#" + "-"* board_width + "#"
     print(header)
 
-    for i in range(board_height):
-        print("|" + " "* board_width + "|")
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots_set: 
+                ch = "X"
+            else:
+                ch = " "
+            row.append(ch)
+
+        print("|" + "".join(row) + "|")
+    
     print(header)
 
 if __name__ == "__main__":
-    render (10, 10)
+    battleships = [
+            Battleship.build ((1,1), 2, "N"),
+            Battleship.build ((5,8), 2, "S"),
+            Battleship.build ((2,3), 4, "E"),
+    ]
 
-print('Hello, Welcome to Battleships2023!')
-ans = input ('Are you ready to play (yes/no)')
-if ans.lower () == 'yes':
-    print('Ok, lets do this' )
-else: 
-    print('Ok, Come back when you are ready') 
+    print(battleships)
 
- #  User friendly interaction before starting game. 
-def where_to_shoot ():
-    inp = input ("Where do you want to shoot on the board?\n")
-    xstr, ystr = insp.split (",")
-    x = int(xstr)
-    y = int(ystr)
-    print (x)
-    print (y)
-    print (x + y)
-
+    exit(0)
     
+    shots = []
+    while True: 
+        inp = input("Where do you want to shoot on the board?\n")
+        xstr, ystr = inp.split (",")
+        x = int(xstr)
+        y = int(ystr)
+        shots.append((x,y))
+        board(10, 10, shots)
