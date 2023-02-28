e9a8701b888ad68c07e290da0b3cf8cbf9692383
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-02-28 14:06:11 +0000

    battleships

--- README.md
+++ README.md
@@ -1,15 +1,42 @@
-1. Battleships, an Interactive game created for the user to have a bit fun trying to find where the battleships are on the board, marked by an X with their "Shot" which they have endless chances to guess. The locations of the fleets are concealed from the other player.
+What is  Battleships?...
+An Interactive game created for the user to have a bit fun trying to find where the battleships are on the board, marked by an X with their "Shot" which they have endless chances to guess. The locations of the fleets are concealed from the other player in this case the user is playing against the computer. 
 
-Simple Game Questions at the beginning for user interaction. 
-
-
- 1. Ask User the question if they would like to play Battleships?..
- 2. If yes. Ask if they would like the rules explained?
- 3. If no..."Take their time & com e back when ready" keeping it user friendly. 
+I've started off my game by having some simple interacation with the User 
 
 
-The first big question is Where do you want to shoot on the board?...To try and shoot the ships. the Board is 10 by 10. 
-The shot is marked by an "X" making it clear to the User the marked shot. 
+ 1. Are you ready to play Battleships?..
+ 2. If yes. Ask if they would like the rules explained?..
+ 3. If no..."Take their time & come back when ready" keeping it user friendly. 
+ 4. If they wish to go straight into it- go straight into the game. 
 
 
+
+
+The big question of the game is...." Where do you want to shoot on the board?"...
+THe board is default 10x10 column and rows.
+But the user has a choice to what size they wish to display their board.
+
+An example answer to the qeustions would looke like this- "1,1"/ "4,7"
+
+a "*" represents a miss
+a "X" represents a hit.
+
+
+
+Overall Stucture 
+1. User interaction
+
+2. Make the board 
+
+3. Get it into X & Y cordinates
+
+4. Display a board with shots
+
+5. Decided how to represent the battleships
+
+6. Decide overall game state
+
+7. Work out when a shot is a hit or a miss 
+
+8. Game over loop 
 
--- run.py
+++ run.py
@@ -1,10 +1,65 @@
 
+# Import random package to be able to create random integers within the game
 import random 
 
-# Import random package to be able to create random integers within the game
+#  User friendly interaction before starting game.
 
- #  User friendly interaction before starting game.
+print('Hello, Welcome to Battleships2023!')
+ans = input ('Are you ready to play (yes/no)')
+if ans.lower () == 'yes':
+    print('Ok, do you want to review the rules before starting?' )
+if ans.lower () == 'yes':
+print('Ok, Come back when you are ready') 
+
+# Create a class for the gameboard 
+
+class GameBoard(object):
+
+    def __init__(self, battleships, board_width, board_height):
+        self.battleships = battleships
+        self.shots = []
+        self.board_width = board_width 
+        self.board_height = board_height
+
+ # * Update the battleships with any shots to them. 
+ # * Save after wether the shot was a hit or a miss.  
+    
+    def take_shot(self, shot_location):
+        hit_battleship = None
+        is_hit = False
+        for b in self.battleships:
+            idx = b.body_index(shot_location)
+            if idx is not None:
+                is_hit = True
+                b.hits[idx] = True
+                hit_battleship = b
+                break
+        
+        self.shots.append(Shot(shot_location, is_hit))
+        return hit_battleship
+
+    def is_game_over(Self):
+        for b in self.battleships: 
+            if not b.is_destroyed():
+                return False 
+        return True 
+
+        # For each battleship, is it destroyed? 
+        # If yes for all, return True, else False 
+
+class Shot (object): 
+
+    def __init__(self, location, is_hit):
+        self.location = location
+        self.is_hit = is_hit
+
+
+
+ #Battleship Building & layout. 
+
+
 class Battleship(object):  
+
     @staticmethod
     def build(head, length, direction):
         body = []
@@ -19,15 +74,57 @@ class Battleship(object):
                 el = (head[0] + i, head[1])
 
             body.append(el)
-        return Battleship(body)
+
+        return Battleship(body,)
 
     def __init__(self, body):
         self.body = body 
+        self.hits = [False] * len(body)
 
-b = Battleship.build((1,1), 5, "S")
-b2 = Battleship ([(1,2), (2,2), (3,3), (4,2), (5,2)])
+    def body_index(self, location):
+        try:
+            return self.body.index(location)
+        except ValueError:
+            return None 
+
+    def is_destroyed(self):
+        return all(self.hits)
+
+#Game Render 
+def render(game_board, show_battleships=False):
+    header  = "#" + "-"* game_board.board_width + "#"
+    print(header)
+
+    # Construct empty board 
+    board = []
+    for _ in range(game_board.board_width):
+        board.append([None for _ in range (game_board.board_height)])
+    if show_battleships:
+
+    # Add the battleships to the board 
+        for b in game_board.battleships:
+         for x, y in b.body:
+            board[x][y] = "0"
+
+    # Add the shots to the board
+    for sh in game_board.shots: 
+        x, y = sh.location
+        if sh.is_hit:
+            ch = "X"
+        else: 
+            ch = "*"
+        board [x][y] = ch 
 
 
+    for y in range(game_board.board_height): 
+        row = []
+        for x in range(game_board.board_width):
+            row.append(board[x][y] or " ")
+        print("|" + "".join(row) + "|")
+    print(header)
+
+
+#Just specifically for the position of the board
 
 def board (board_width, board_height, shots): 
     header  = "#" + "-"* board_width + "#"
@@ -46,43 +143,34 @@ def board (board_width, board_height, shots):
         print("|" + "".join(row) + "|")
     
     print(header)
-def board_battleships(board_width, board_height, battleships):
-    header  = "#" + "-"* board_width + "#"
-
-    print(header)
-
-    # Construct empty board 
-    board = []
-    for x in range(board_width):
-        board.append([None for x in range (board_height)])
-
-    # Add the battleships to the board 
-    for b in battleships:
-        for x,y in b.body:
-            board [x][y] = "0"
-
-    print (board)
-    print (header)
+# Sorting out the end of the game loop. 
 
 if __name__ == "__main__":
     battleships = [
             Battleship.build ((1,1), 2, "N"),
-            Battleship.build ((5,8), 2, "N"),
-            Battleship.build ((2,3), 4, "E"),
+            #Battleship.build ((5,8), 2, "N"),
+           # Battleship.build ((2,3), 4, "E"),
     ]
 
     for b in battleships:
         print(b.body)
-        board_battleships(10,10, battleships),
-    exit(0)
-
-
     
-    shots = []
+    game_board = GameBoard(battleships, 10, 10)
+
     while True: 
-        inp = input("Where do you want to shoot on the board?\n")
+        inp = input("Whereabouts do you want to shoot your misile onto the board?\n")
         xstr, ystr = inp.split (",")
         x = int(xstr)
         y = int(ystr)
-        shots.append((x,y))
-        board(10, 10, shots)
+
+        game_board.take_shot((x,y))
+        render(game_board)
+
+        if game_board.is_game_over():
+            print("YOU WIN!")
+            break
+
+
+        #if game_has_ended: 
+        #   print "YOU WIN"
+        #   break 
