bb7ff31207043427ba7a5efdbc0c495175c2d9cb
sammaxfleet <samuelfleet@yahoo.co.uk>
2023-03-03 10:26:41 +0000

    added shots to the board with a character

--- run.py
+++ run.py
@@ -5,30 +5,39 @@ def run_game():
     print ("Hi welcome to Battleships 2023!") 
     ans = input ("Do you wish to review the rules before playing? (yes/no)")
     if ans.lower () == 'yes':
-            print("Rules...\n",".The Battleship Board has a 10 by 10 layout \n", ".To take your shot at the board type a number followed a comma follwed by another number, example 1,1 \n""The battleships are invisible, the board will tell you if your shot has hit by and X or a missed shot will be represented by a * \n " ".You can play as many times as you wish.")
+            print("Rules...\n","1.The Battleship Board has a 10 by 10 layout \n", "2.To take your shot at the board type a number followed a comma  follwed by another number, example.   1,1 \n", "3.The battleships are invisible, the board will tell you if your shot has hit by using an X... a missed shot will be represented by a * \n " "4..You can play as many times as you wish.")
+        
     elif ans == "no":
-            print ("Come back when you're ready")
-#Board Layout 
+            print ("Ok, come back when you're ready.")
+run_game()    
 
-def board_layout(board_width, board_height):
+#Board Layout & Shots.
+
+def board_layout(board_width, board_height, shots):
     print("#" + "-" * board_width + "#")
 
-    for i in range(board_height):
-        
-        print("|" + " " * board_width + "|")
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots:
+                ch = "4"
+            else:
+                ch = " "
+            row.append(ch)
+        print("|" + "".join(row) + "|")
+
+        #print("|" + " " * board_width + "|")
     
     footer = ("#" + "-" * board_width + "#")
-    print (footer)
+    print(footer)
 
-        #User interaction 2 & shots 
-def play_turn():
-    board_layout (10,10)
+
+
+        #User interaction 2 & continuation of plotting board & shots 
+if __name__ == "__main__":
+    board_layout (10,10, [(3,1), (4,5), (8,1)])
+
     inp= input ("Where on the board do you want to shoot your missile?..\n")
     xstr, ystr = inp.split (",")
     x = int(xstr)
     y = int(ystr)
-    print (x)
-    print (y)
-    print (x+y)
-
-run_game()
