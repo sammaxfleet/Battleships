e74aa82ea30150153968788c778d14275a62370e
sammaxfleet <>
2023-08-01 16:08:07 +0000

    added user guess & shots on the board

--- run.py
+++ run.py
@@ -39,3 +39,58 @@ def place_user_ships(grid):
                     print("Invalid input. Try again.")
             except ValueError:
                 print("Invalid input. Try again.")
+
+
+
+
+def place_computer_ship(grid):
+    NUM_COMPUTER_SHIPS = 3
+    for _ in range(NUM_COMPUTER_SHIPS):
+        row = random.randint(0, GRID_SIZE - 1)
+        col = random.randint(0, GRID_SIZE - 1)
+        place_ship(grid, row, col)
+
+def get_user_guess():
+    try:
+        guess_row = int(input(f"Guess Row (0-{GRID_SIZE-1}): "))
+        guess_col = int(input(f"Guess Col (0-{GRID_SIZE-1}): "))
+        if 0 <= guess_row < GRID_SIZE and 0 <= guess_col < GRID_SIZE:
+            return guess_row, guess_col
+        else:
+            print("Oops, that's not even in the ocean! Try again.")
+            return get_user_guess()
+    except ValueError:
+        print("Invalid input. Please enter numbers between 0 and 7.")
+        return get_user_guess()
+
+
+def is_hit(grid, row, col):
+    return grid[row][col] == 'X'
+
+def play_game(grid):
+    print("Let's play Battleships!")
+    print("Try to sink the computer's ships.")
+    while True:
+        print_grid(grid)
+        user_guess_row, user_guess_col = get_user_guess()
+        
+        if is_hit(grid, user_guess_row, user_guess_col):
+            print("Congratulations! You hit the computer's ship!")
+            grid[user_guess_row][user_guess_col] = 'H'
+        else:
+            print("You missed the computer's ship!")
+            grid[user_guess_row][user_guess_col] = 'M'
+
+if __name__ == "__main__":
+    # Allow the user to set the grid size
+    GRID_SIZE = int(input("Enter the grid size (e.g., 8): "))
+    
+    # Create the game grid
+    grid = create_grid(GRID_SIZE)
+    
+    # Place user and computer ships
+    place_user_ships(grid)
+    place_computer_ship(grid)
+    
+    # Play the game
+    play_game(grid)
